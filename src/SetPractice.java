761a32011eab0e2be4578eb86942a01278e1de26
tim <timmywilliams4665@gmail.com>
2025-01-07 20:43:32 -0800

    Finished most of work one file to go

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,17 +1,35 @@
+import java.util.HashSet;
+
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
+    HashSet<String> hashSetValue = new HashSet<String>();
 
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
+    hashSetValue.add("red");
+    hashSetValue.add("blue");
+    hashSetValue.add("green");
 
     // Check whether the Set contains a given String
 
+    System.out.println(hashSetValue.contains("red"));
+
     // Remove an element from the Set
+
+    System.out.println(hashSetValue);
+    hashSetValue.remove("red");
+    System.out.println(hashSetValue);
 
     // Get the size of the Set
 
+    System.out.println(hashSetValue.size());
+
     // Iterate over the elements of the Set, printing each one on a separate line
+    for (String iterate : hashSetValue)
+    {
+      System.out.println(iterate);  
+    }
 
     /*
      * Warning!
--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,25 +1,51 @@
+import java.util.ArrayList;
+
 public class StringPractice {
   public static void main(String[] args) {
     // Create a string with at least 5 characters and assign it to a variable
+    String letters = "Hello World";
+    String AssignString = letters;
+
+    System.out.println(AssignString);
 
     // Find the length of the string
 
+    System.out.println(AssignString.length());
+
     // Concatenate (add) two strings together and reassign the result
+    String Value1 = "hel";
+    String Value2 = "lo";
+
+    System.out.println(Value1 + Value2);
 
     // Find the value of the character at index 3
+    System.out.println(AssignString.charAt(3));
 
     // Check whether the string contains a given substring (i.e. does the string have "abc" in it?)
+    System.out.println(AssignString.contains("Hell"));
 
     // Iterate over the characters of the string, printing each one on a separate line
+    for(int i = 0; i < AssignString.length(); i++)
+    {
+      System.out.println(AssignString.charAt(i));
+    }
 
     // Create an ArrayList of Strings and assign it to a variable
+    ArrayList<String> ValueList = new ArrayList<String>();
 
     // Add multiple strings to the List (OK to do one-by-one)
+    ValueList.add("Blue");
+    ValueList.add("Red");
+    ValueList.add("Green");
+    ValueList.add("Yellow");
 
     // Join all of the strings in the list together into a single string separated by commas
     // Use a built-in method to achieve this instead of using a loop
+    String storeValues = String.join(",", ValueList);
+    System.out.println(storeValues);
 
     // Check whether two strings are equal
+    System.out.println(Value1.equals(Value2));
 
     /*
      * Reminder!
